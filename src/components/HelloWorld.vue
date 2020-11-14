c6ab5f309984bc97470a36d2b3d90e52f3052718
Clifford <ts0260140@gmail.com>
2020-11-14 23:31:17 +0800

    feat(reactive): composition api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,22 @@
   <div>
     <div>{{ count }}</div>
     <button @click="myFn">點擊</button>
+
+    <ul>
+      <li
+        v-for="(stu, index) in state.stus"
+        :key="stu.id"
+        @click="remStu(index)"
+      >
+        {{ stu.name }}-{{ stu.age }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+// ref只能監聽簡單的類型,reactive能監聽 物件、陣列
+import { ref, reactive } from 'vue';
 export default {
   name: 'HelloWorld',
 
@@ -15,9 +26,39 @@ export default {
 
     let myFn = () => {
       // console.log(count);
-      count.value += 1; 
+      count.value += 1;
     };
-    return { count, myFn };
+
+    let { state, remStu } = useRemoveStudent();
+
+    // let state = reactive({
+    //   stus: [
+    //     { id: 1, name: 'aa', age: 10 },
+    //     { id: 2, name: 'bb', age: 20 },
+    //     { id: 3, name: 'cc', age: 30 }
+    //   ]
+    // });
+
+    // let remStu = index => {
+    //   state.stus = state.stus.filter((item, idx) => idx !== index);
+    // };
+    return { count, myFn, state, remStu };
   }
 };
+
+function useRemoveStudent() {
+  let state = reactive({
+    stus: [
+      { id: 1, name: 'aa', age: 10 },
+      { id: 2, name: 'bb', age: 20 },
+      { id: 3, name: 'cc', age: 30 }
+    ]
+  });
+
+  let remStu = index => {
+    state.stus = state.stus.filter((item, idx) => idx !== index);
+  };
+
+  return { state, remStu };
+}
 </script>
