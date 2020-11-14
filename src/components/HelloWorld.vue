c4885e9da2c51c3d7d67982be246e16a31fbab54
Clifford <ts0260140@gmail.com>
2020-11-14 20:01:19 +0800

    feat(init): 初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>{{ count }}</div>
+    <button @click="myFn">點擊</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'HelloWorld',
+
+  setup() {
+    let count = ref(0);
+
+    let myFn = () => {
+      // console.log(count);
+      count.value += 1; 
+    };
+    return { count, myFn };
+  }
+};
+</script>
